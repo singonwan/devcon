810cf199805b1605dc9d623cddb9a8bdf0e21e7d
George Wan <singonwan@gmail.com>
2019-10-02 17:30:31 -0400

    created route files with express router

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+//define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
 });
