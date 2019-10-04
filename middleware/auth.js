9cddc75a8e26b190de5aeaf11f2a08796dec08aa
George Wan <singonwan@gmail.com>
2019-10-04 11:03:45 -0400

    created custom auth middleware with jwt in middleware authjs

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+//all middleware take in req res and next. does something with req res and then next to continue after middleware.
+
+module.exports = function(req, res, next) {
+    // get token from header
+    const token = req.header('x-auth-token');
+
+    //check if token exists
+    if (!token) {
+        //status 401 = not authorized
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    //verify token
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user; //setting it to id from mongo after decoding token
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'token is not valid' });
+    }
+};
