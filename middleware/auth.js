be6e8f32fc48c2420adb26893f597143cafe251d
George Wan <singonwan@gmail.com>
2019-10-04 12:04:51 -0400

    added auth middleware to api auth route to test and send back user object minus password if token is verified

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,7 +17,7 @@ module.exports = function(req, res, next) {
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-        req.user = decoded.user; //setting it to id from mongo after decoding token
+        req.user = decoded.user; //setting it to user object only if verified
         next();
     } catch (err) {
         res.status(401).json({ msg: 'token is not valid' });
