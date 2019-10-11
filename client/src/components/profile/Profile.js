80344f5d2d1e57a2b5003d1845465b1cc36ba377
George Wan <singonwan@gmail.com>
2019-10-11 17:43:32 -0400

    added experience display on profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 const Profile = ({
     match,
@@ -37,6 +38,21 @@ const Profile = ({
                     <div className="profile-grid my-1">
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {profile.experience.length > 0 ? (
+                                <Fragment>
+                                    {profile.experience.map(exp => (
+                                        <ProfileExperience
+                                            key={exp._id}
+                                            experience={exp}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No Prior Experiences</h4>
+                            )}
+                        </div>
                     </div>
                 </Fragment>
             )}
