457c318391f5f0651b3f6fed96fecff0cd0af0b6
George Wan <singonwan@gmail.com>
2019-10-11 17:11:39 -0400

    added about page, listing bio and skills on profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
     match,
@@ -35,6 +36,7 @@ const Profile = ({
                         )}
                     <div className="profile-grid my-1">
                         <ProfileTop profile={profile} />
+                        <ProfileAbout profile={profile} />
                     </div>
                 </Fragment>
             )}
