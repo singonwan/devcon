474408e7aa74609a80ef715a3223aba2d8aaf9ab
George Wan <singonwan@gmail.com>
2019-10-11 23:29:20 -0400

    created reducer, action and component for posts

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+
+    return <div></div>;
+};
+
+Posts.propTypes = {
+    post: PropTypes.object.isRequired,
+    getPosts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    post: state.post
+});
+
+export default connect(
+    mapStateToProps,
+    { getPosts }
+)(Posts);
